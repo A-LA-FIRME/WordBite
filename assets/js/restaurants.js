e12862430d52094e5165b6c1b15499f8b101c20f
martinez.jose.2915@gmail.com <martinez.jose.2915@gmail.com>
2023-10-22 20:20:15 -0500

    Names and numbers restaurant

--- assets/js/restaurants.js
+++ assets/js/restaurants.js
@@ -11,18 +11,24 @@ var Restaurants = {};
     drawRestaurants: function () {
       var restaurantsArr = [
         {
-          ubication: "Ubication 1",
-          phone: "234-5678",
+          ubication: "Los Andes Mall",
+          phone: "231-3100",
           image_url: "../assets/img/about.jpg",
           maps_ubication: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5920.708673009257!2d1.8411223486681627!3d42.09988148487294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a508312251967d%3A0xc8dac010a7b1e524!2s08600%20Berga%2C%20Barcelona%2C%20Espa%C3%B1a!5e0!3m2!1ses-419!2spa!4v1697936002404!5m2!1ses-419!2spa" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
         },
         {
-          ubication: "Ubication 2",
-          phone: "234-5678",
+          ubication: "San Miguelito La Gran Estación",
+          phone: "231-5000",
           image_url: "../assets/img/about.jpg",
           maps_ubication: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5920.708673009257!2d1.8411223486681627!3d42.09988148487294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a508312251967d%3A0xc8dac010a7b1e524!2s08600%20Berga%2C%20Barcelona%2C%20Espa%C3%B1a!5e0!3m2!1ses-419!2spa!4v1697936002404!5m2!1ses-419!2spa" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
         }
         // ... Más restaurantes
+        {
+          ubication: "Albrook",
+          phone: "231-2100",
+          image_url: "../assets/img/about.jpg",
+          maps_ubication: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5920.708673009257!2d1.8411223486681627!3d42.09988148487294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a508312251967d%3A0xc8dac010a7b1e524!2s08600%20Berga%2C%20Barcelona%2C%20Espa%C3%B1a!5e0!3m2!1ses-419!2spa!4v1697936002404!5m2!1ses-419!2spa" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+        }
       ];
 
       var restaurantsHtml = "";
